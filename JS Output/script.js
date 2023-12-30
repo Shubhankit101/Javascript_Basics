9fe0740706a43541cb802c057d6b26e2ef75ab22
Shubhankit101 <45731902+Shubhankit101@users.noreply.github.com>
2023-12-30 19:31:10 +0530

    basics js output methods

---
+++ JS Output/script.js
@@ -0,0 +1,11 @@
+//Writes into an HTML element using innerHTML
+document.getElementById("demo").innerHTML = 5 + 7;
+
+//Writes HTML output on Browser, Should only be used for testing
+//document.write(5 + 6);
+
+//Writes into an alert box
+//window.alert(9+9);
+
+//Writes into Console
+console.log(13+48);
